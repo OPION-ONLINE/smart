b20e2554c61645c62833165cc4c04550f5ad85de
codesmith-247 <adedavid.tech@gmail.com>
2022-07-01 16:57:17 -0400

    made changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/me/newscratch/templates/configuration.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/me/newscratch/templates/configuration.php';
 
 $pages = $environment_link . 'templates/pages/';
 
