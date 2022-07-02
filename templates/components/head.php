8595b4f2124606da9e86721002e11a3cb87657ae
divi-nation <oseidivine35@gmail.com>
2022-07-02 00:02:51 +0000

    done with car

--- templates/components/head.php
+++ templates/components/head.php
@@ -5,3 +5,4 @@
 <link rel = 'stylesheet' href = '<?php echo $environment_link . 'styles/general.css';?>'>
 <link rel = 'stylesheet' href = '<?php echo $environment_link . 'styles/variables.css';?>'>
 <link rel = 'stylesheet' href = '<?php echo $environment_link . 'styles/components/navigation.css';?>'>
+<link rel=  'stylesheet' href=  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css'>
